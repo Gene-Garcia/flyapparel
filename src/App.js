8e157d92bbb70b67792a3b2e0428985ef4dc0065
gn-grc <genejogarcia.gg@gmail.com>
2021-10-24 16:18:29 +0800

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Fly Apparel</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="">
+        <nav className="bg-blue-500 h-10 shadow">Hello</nav>
       </header>
     </div>
   );
